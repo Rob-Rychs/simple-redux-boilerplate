0ca55934e4dd2c3900f5957b7af17411bd9853d5
Abhishek Ghosh <abhighosh18@gmail.com>
2017-07-10 23:22:23 -0700

    Remove: Remove unused package

--- app/client/src/containers/HomePageContainer/index.js
+++ app/client/src/containers/HomePageContainer/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
-import 'whatwg-fetch';
 import PostsList from '../../components/PostsList';
 import Categories from '../../components/Categories';
 import * as HomePageActions from './actions';
