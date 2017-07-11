12669ef470b334f4e8d09065be176a3a9fa0fa8b
Abhishek Ghosh <abhighosh18@gmail.com>
2017-07-10 21:53:55 -0700

    Feat: Add hyperlinks to the category pages

--- app/client/src/components/Categories.jsx
+++ app/client/src/components/Categories.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const CategoriesList = ({ categories }) => (
   <div>
     <ul>
       {categories &&
         categories.map(category => (
-          <li key={category.name}>{category.name}</li>
+          <li key={category.name}>
+            <Link to={`/category/${category.path}`}>{category.name}</Link>
+          </li>
         ))}
     </ul>
   </div>
